12f3d92f2691c110faa57c9d339e9bee40a6e9b6
Eugene Yan <eugeneyan77@gmail.com>
2023-04-12 15:13:01 -0500

    Create game() function which plays a 5 rounds of rock, paper, scissors

--- myScript.js
+++ myScript.js
@@ -10,51 +10,82 @@ function getComputerChoice() {
 }
 
 const computerSelection = getComputerChoice();
-const playerSelection = prompt("Enter rock, paper, or scissors: ")
+const win = "You win!";
+const lose = "You lose!";
+const tie = "It's a tie!";
 
 //function that plays a round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
   //converts strings to lowercase making everything case-insensitive
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
+  const tie = "It's a tie!";
   console.log(playerSelection);
   console.log(computerSelection);
   //if player chooses rock
   if(playerSelection == "rock") {
     if(computerSelection == "rock") {
-      return "It's a tie!";
+      return tie;
     }
     else if(computerSelection == "scissors") {
-      return "You Win! Rock beats Scissors";
+      return win + " Rock beats Scissors";
     }
     else {
-      return "You Lose! Paper beats Rock";
+      return lose + " Paper beats Rock";
     }
   }
   //if player chooses paper
   if(playerSelection == "paper") {
     if(computerSelection == "paper") {
-      return "It's a tie!";
+      return tie;
     }
     else if(computerSelection == "rock") {
-      return "You Win! Paper beats Rock";
+      return win + " Paper beats Rock";
     }
     else {
-      return "You Lose! Scissors beats Paper";
+      return lose + " Scissors beats Paper";
     }
   }
   //if player chooses scissors
   if(playerSelection == "scissors") {
     if(computerSelection == "scissors") {
-      return "It's a tie!";
+      return tie;
     }
     else if(computerSelection == "paper") {
-      return "You Win! Scissors beats Paper";
+      return win + " Scissors beats Paper";
     }
     else {
-      return "You Lose! Rock beats Scissors";
+      return lose + " Rock beast Scissors";
     }
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+//function that plays a 5 round game
+function game() {
+  let rounds;
+  let wins = 0, losses = 0, ties = 0;
+  for(let i = 0; i <= 4; i++) {
+    let playerSelection = prompt("Enter rock, paper, or scissors: ")
+    rounds = playRound(playerSelection, computerSelection);
+    console.log(rounds);
+    if(rounds == win) {
+      wins++;
+    }
+    else if(rounds == lose) {
+      losses++;
+    }
+    else {
+      ties++;
+    }
+  }
+  if(wins > losses) {
+    return "Congratulations! You won!";
+  }
+  else if(losses > wins) {
+    return "Sorry! You lost!";
+  }
+  else {
+    return "You tied! Try again!";
+  }
+}
+console.log(game());
