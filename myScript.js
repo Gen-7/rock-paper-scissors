b8472c93420554ba16b504553c5d5072b6493ca6
Eugene Yan <eugeneyan77@gmail.com>
2023-04-12 14:41:24 -0500

    Create function that plays a round of rock, paper, scissors

--- myScript.js
+++ myScript.js
@@ -1,17 +1,60 @@
-//Start with function getComputerChoice
-//getComputerChoice randomly returns either 'Rock', 'Paper', or 'Scissors'
-const random = ['Rock', 'Paper', 'Scissors'];
+const random = ["Rock", "Paper", "Scissors"];
 
-function getComputerChoice(){
-  //get index value from random
+//computer randomly chooses rock, paper, or scissors 
+function getComputerChoice() { 
+  //get random index value from array
   const index = Math.floor(Math.random() * random.length);
-  //choose random
+  //choose randomly
   const chosen = random[index];
   return chosen;
 }
- 
-console.log(getComputerChoice());
-//function that plays a single round of RPS
-//function should take 2 parameters: playerSelection and computerSelection
-//function returns a string that declares the winner of the round
-//make playerSelection cas-insensitive
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Enter rock, paper, or scissors: ")
+
+//function that plays a round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+  //converts strings to lowercase making everything case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  console.log(playerSelection);
+  console.log(computerSelection);
+  //if player chooses rock
+  if(playerSelection == "rock") {
+    if(computerSelection == "rock") {
+      return "It's a tie!";
+    }
+    else if(computerSelection == "scissors") {
+      return "You Win! Rock beats Scissors";
+    }
+    else {
+      return "You Lose! Paper beats Rock";
+    }
+  }
+  //if player chooses paper
+  if(playerSelection == "paper") {
+    if(computerSelection == "paper") {
+      return "It's a tie!";
+    }
+    else if(computerSelection == "rock") {
+      return "You Win! Paper beats Rock";
+    }
+    else {
+      return "You Lose! Scissors beats Paper";
+    }
+  }
+  //if player chooses scissors
+  if(playerSelection == "scissors") {
+    if(computerSelection == "scissors") {
+      return "It's a tie!";
+    }
+    else if(computerSelection == "paper") {
+      return "You Win! Scissors beats Paper";
+    }
+    else {
+      return "You Lose! Rock beats Scissors";
+    }
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
